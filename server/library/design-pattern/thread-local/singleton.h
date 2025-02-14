9d8cc3da032b350f60e906223a1c3be363b4c25a
user <user@DESKTOP-2PAL0L0>
2025-02-14 20:19:00 +0900

    250214

--- server/library/design-pattern/thread-local/singleton.h
+++ server/library/design-pattern/thread-local/singleton.h
@@ -8,10 +8,10 @@ namespace design_pattern::_thread_local {
 		inline explicit singleton(void) noexcept = default;
 		inline ~singleton(void) noexcept = default;
 	private:
-		inline explicit singleton(singleton const& rhs) noexcept = delete;
-		inline explicit singleton(singleton&& rhs) noexcept = delete;
-		inline auto operator=(singleton const& rhs) noexcept -> singleton & = delete;
-		inline auto operator=(singleton&& rhs) noexcept -> singleton & = delete;
+		inline explicit singleton(singleton const&) noexcept = delete;
+		inline explicit singleton(singleton&&) noexcept = delete;
+		inline auto operator=(singleton const&) noexcept -> singleton & = delete;
+		inline auto operator=(singleton&&) noexcept -> singleton & = delete;
 	public:
 		inline static auto instance(void) noexcept -> type& {
 			thread_local type _instance;
