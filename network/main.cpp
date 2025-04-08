732aa901bb52269575fbc2013c2c86bc089439c0
user <user@DESKTOP-2PAL0L0>
2025-04-08 19:18:17 +0900

    250408

--- network/main.cpp
+++ network/main.cpp
@@ -1,9 +1,7 @@
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
-
-#include "network.hpp"
-#include "server.h"
+//#include "network"
 
 int main(void) noexcept {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
