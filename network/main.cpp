ba114df048161ae96a98052059dcd1ba1dc2fb28
oltex <80347929+oltex@users.noreply.github.com>
2025-04-09 18:39:51 +0900

    250409

--- network/main.cpp
+++ network/main.cpp
@@ -1,14 +1,21 @@
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
-#include "framework/network.hpp"
+#include "echo_server.h"
 
 int main(void) noexcept {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	network& network_ = network::instance();
-	
-	network_.start();
-	server_.accept("127.0.0.1", 6000, 0, 65535);
-	Sleep(INFINITE);
+	echo_server _server;
+	{
+		command::parameter param("include", "server.cfg");
+		command::instance().execute("include", &param);
+	}
+	system("pause");
+	{
+		command::parameter param("server_stop");
+		command::instance().execute("server_stop", &param);
+	}
+	system("pause");
+	//Sleep(INFINITE);
 }
